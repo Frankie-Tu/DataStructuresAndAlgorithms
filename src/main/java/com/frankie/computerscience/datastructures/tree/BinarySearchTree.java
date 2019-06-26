ff3bd6ecd2e942b9798466011e60154091868642
Frankie Tu <hua.tu@hotmail.com>
2019-06-26 19:05:31 -0400

    added BinarySearchTree delete() method and test cases

--- src/main/java/com/frankie/computerscience/datastructures/tree/BinarySearchTree.java
+++ src/main/java/com/frankie/computerscience/datastructures/tree/BinarySearchTree.java
@@ -42,4 +42,41 @@ public class BinarySearchTree extends BinaryTree {
         }
         return null;
     }
+
+    public Node findSuccessor(Node node) {
+        Node returnNode = node;
+        while (returnNode.getLeft() != null){
+            returnNode = node.getLeft();
+        }
+        return returnNode;
+    }
+
+    public void delete(int val) {
+        root = delete(root, val);
+    }
+
+    private Node delete(Node node, int val) {
+        if (node.getData() > val && node.getLeft() != null) {
+            node.setLeft(delete(node.getLeft(), val));
+        } else if (node.getData() < val && node.getRight() != null) {
+            node.setRight(delete(node.getRight(), val));
+        } else { // If the node to be deleted is found or value not found in the tree at the end of traversal
+            if (node.getLeft() != null && node.getRight() != null) { // has two children
+                // the next min Node
+                Node successor = findSuccessor(node.getRight());
+                // copy the successor data over to the node we want to delete
+                node.setData(successor.getData());
+                // take successor node out of right child
+                node.setRight(delete(node.getRight(), successor.getData()));
+            } else if (node.getLeft() != null) { // has left child
+                return node.getLeft();
+            } else if (node.getRight() != null) { // has right child
+                return node.getRight();
+            } else { // has no children
+                return null;
+            }
+        }
+        // return the updated binary search tree
+        return node;
+    }
 }
--- src/test/java/com/frankie/computerscience/datastructures/tree/BinarySearchTreeTest.java
+++ src/test/java/com/frankie/computerscience/datastructures/tree/BinarySearchTreeTest.java
@@ -55,4 +55,39 @@ public class BinarySearchTreeTest {
         assertEquals(myTree.search(2).getData(), 2);
         assertNull(myTree.search(10));
     }
+
+    @Test
+    public void delete() {
+        /*
+                    3
+                  /   \
+                 2     5
+                /    /  \
+               1    4    7
+                          \
+                           8
+         */
+
+        myTree.insert(new Node(3));
+        myTree.insert(new Node(5));
+        myTree.insert(new Node(7));
+        myTree.insert(new Node(2));
+        myTree.insert(new Node(1));
+        myTree.insert(new Node(4));
+        myTree.insert(new Node(8));
+
+
+        System.out.println("One child delete test: should print 1,2,3,4,5,8");
+        myTree.delete(7);
+        myTree.inorderTraversal();
+
+
+        System.out.println("Leaf node delete test: should print 2,3,4,5,8");
+        myTree.delete(1);
+        myTree.inorderTraversal();
+
+        System.out.println("Two children delete test: should print 2,3,4,8");
+        myTree.delete(5);
+        myTree.inorderTraversal();
+    }
 }
